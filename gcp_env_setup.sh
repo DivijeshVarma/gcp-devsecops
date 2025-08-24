1cc03795caedaaf2bbdc57ec4434d598914464d7
divijesh <divijesh@divijeshs-Mac-mini.local>
2025-08-24 12:53:52 +0530

    First try

---
+++ gcp_env_setup.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+
+#Run the following ONE-TIME-SCRIPT which creates and provisions the necessary GCP cloud services that will be required to create the DevSecOps CICD pipeline for a sample docker application. Here's all the service deployments that will occur once the script finishes:
+
+
+#Enable the following GCP APIs
+#Cloud Build, Binary Authorization, On-Demand Scanning, Resource Manager API, Artifact Registry API, Artifact Registry Vulnerability Scanning, Cloud Deploy API, KMS API and Cloud Functions.
+gcloud services enable cloudbuild.googleapis.com
+gcloud services enable container.googleapis.com
+gcloud services enable ondemandscanning.googleapis.com
+gcloud services enable cloudresourcemanager.googleapis.com
+gcloud services enable artifactregistry.googleapis.com
+gcloud services enable containerscanning.googleapis.com
+gcloud services enable clouddeploy.googleapis.com
+gcloud services enable storage.googleapis.com
+gcloud services enable secretmanager.googleapis.com
+
+
+#GCP Project Variables
+LOCATION=asia-south1
+PROJECT_ID=devsecops12
+PROJECT_NUMBER=429087103686
+CLOUD_BUILD_SA_EMAIL="${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com"
+COMPUTE_SA="${PROJECT_NUMBER}-compute@developer.gserviceaccount.com"
+
+#Create the following custom IAM role
+gcloud iam roles create cicdblogrole --project=${PROJECT_ID} \
+    --title="cicdblogrole" \
+    --description="Custom Role for GCP CICD Blog" \
+    --permissions="artifactregistry.repositories.create,container.clusters.get,clouddeploy.deliveryPipelines.get,clouddeploy.releases.get,containeranalysis.notes.attachOccurrence,containeranalysis.notes.create,containeranalysis.notes.listOccurrences,containeranalysis.notes.setIamPolicy,iam.serviceAccounts.actAs,ondemandscanning.operations.get,ondemandscanning.scans.analyzePackages,ondemandscanning.scans.listVulnerabilities,serviceusage.services.enable,storage.objects.get" \
+    --stage=Beta
+
+#Add the newly created custom role, and "Cloud Deploy Admin" to the Cloud Build Service Account
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member="serviceAccount:${CLOUD_BUILD_SA_EMAIL}" --role="projects/${PROJECT_ID}/roles/cicdblogrole"
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member="serviceAccount:${CLOUD_BUILD_SA_EMAIL}" --role='roles/secretmanager.secretAccessor'
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member="serviceAccount:${CLOUD_BUILD_SA_EMAIL}" --role='roles/clouddeploy.admin'
+
+#Add the following: "Artifact Registry Reader", "Cloud Deploy Runner" and "Kubernetes Engine Admin" IAM Role to the Compute Engine Service Account
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member="serviceAccount:${COMPUTE_SA}" --role='roles/artifactregistry.reader'
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member="serviceAccount:${COMPUTE_SA}" --role='roles/clouddeploy.jobRunner'
+
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member="serviceAccount:${COMPUTE_SA}" --role='roles/container.admin'
+
+#Create Artifact Registry Repository where images will be stored
+gcloud artifacts repositories create test-repo \
+    --repository-format=Docker \
+    --location=$LOCATION \
+    --description="Artifact Registry for GCP DevSecOps CICD Blog" \
+    --async
+
+# Create Bucket for DAST scan
+
+BUCKET_NAME="bucket-$(date +%s)"
+gcloud storage buckets create "gs://$BUCKET_NAME" --location=$LOCATION
+
+#This plugin is required for your kubectl command-line tool to authenticate with the GKE clusters.
+gcloud components install gke-gcloud-auth-plugin
+
+
+# Create secrets for sonar token
+gcloud secrets create sonar-token --replication-policy="automatic" --data-file=sonar-token.txt
+
+#Create three GKE clusters for test, staging and production. The Node.js docker image will be deployed as a release through the Cloud Deploy pipeline first in "dev". Next, the image deployment will be rolled to the "staging" cluster and once its successful, pending approval, the final image roll-out will deploy to the "prod" cluster.
+#NOTE: If you're using a different VPC, ensure you change the --subnetwork config value to match against your VPC subnet
+
+#GKE Cluster for Test environment, uncomment --subnetwork if you want to use a non-default VPC
+gcloud container clusters create test \
+    --project=$PROJECT_ID \
+    --machine-type=e2-medium \
+    --region $LOCATION \
+    --num-nodes=1 \
+    --binauthz-evaluation-mode=PROJECT_SINGLETON_POLICY_ENFORCE \
+    --labels=app=vulnapp-test \
+    --subnetwork=default \
+    --disk-size=20
+
+STATIC_IP=$(gcloud compute addresses create ingress-ip --region=$LOCATION --project=$PROJECT_ID --format='value(address)')
+
+#GKE Cluster for Staging environment
+gcloud container clusters create staging \
+    --project=$PROJECT_ID \
+    --machine-type=e2-medium \
+    --region $LOCATION \
+    --num-nodes=1 \
+    --binauthz-evaluation-mode=PROJECT_SINGLETON_POLICY_ENFORCE \
+    --labels=app=vulnapp-staging \
+    --subnetwork=default \
+    --disk-size=20
+
+#GKE Cluster for Production environment
+gcloud container clusters create prod \
+    --project=$PROJECT_ID \
+    --machine-type=e2-medium \
+    --region $LOCATION \
+    --num-nodes=1 \
+    --binauthz-evaluation-mode=PROJECT_SINGLETON_POLICY_ENFORCE \
+    --labels=app=vulnapp-prod \
+    --subnetwork=default \
+    --disk-size=20
+
+#Create cloud deploy pipeline
+gcloud deploy apply --file clouddeploy.yaml --region=$LOCATION --project=$PROJECT_ID
+
+# Install NGINX Ingress Controller on the 'test' cluster
+gcloud container clusters get-credentials test --region $LOCATION --project $PROJECT_ID
+
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.service.loadBalancerIP=$STATIC_IP
+
+# Install NGINX Ingress Controller on the 'staging' cluster
+gcloud container clusters get-credentials staging --region $LOCATION --project $PROJECT_ID
+
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+
+# Install NGINX Ingress Controller on the 'prod' cluster
+gcloud container clusters get-credentials prod --region $LOCATION --project $PROJECT_ID
+
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+
+echo "====================="
+
+# Display the static IP address on the screen
+echo "The static IP address created is: $STATIC_IP"
+
+# Display Bucket Name
+echo "$BUCKET_NAME"
+
+echo "====================="
